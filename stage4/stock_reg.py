e2e15a37bb392ad8f722788aa0f613d28b3eefa7
LikeJust <diddydwns135@gmail.com>
2018-01-01 17:39:22 +0900

    main function edit

--- stage4/stock_reg.py
+++ stage4/stock_reg.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 from RnnModel import RnnModel
 from DataLoad import DataLoader
 import matplotlib.pyplot as plt
+import os
 
 flag = tf.app.flags
 flag.DEFINE_integer('iterations', 2000, 'iterations')
 flag.DEFINE_integer('seq_length', 7, 'seq_length')
+flag.DEFINE_string('csv_path', 'csv_data', 'csv data path')
 FLAGS = flag.FLAGS
 
 
@@ -17,24 +19,34 @@ def visual_graph(test_y, test_predict):
     plt.show()
 
 
-def main(_):
-    sess = tf.Session()
-    graph = RnnModel(sess, 'rnn')
-    data_loader = DataLoader(FLAGS)
+def train_fn(sess, graph, loader):
     sess.run(tf.global_variables_initializer())
-    train_x, train_y = data_loader.get_train()
+    train_x, train_y = loader.get_train()
 
     for i in range(FLAGS.iterations):
         _, step_loss = graph.train(train_x, train_y)
         print("[step: {}] loss: {}".format(i, step_loss))
 
-    test_x, test_y = data_loader.get_test()
+
+def prediction_stock(graph, loader):
+    test_x, test_y = loader.get_test()
     test_prediction = graph.test(test_x)
     visual_graph(test_y, test_prediction)
 
-    last_data = data_loader.get_last()
+    last_data = loader.get_last()
     last_prediction = graph.test(last_data)
-    data_loader.last_data_process(last_prediction)
+    prediction, direction = loader.last_data_process(last_prediction)
+
+    print(prediction, direction)
+
+
+def main(_):
+    sess = tf.Session()
+    graph = RnnModel(sess, 'rnn')
+    data_loader = DataLoader('csv_data/gears.csv', FLAGS)
+
+    train_fn(sess, graph, data_loader)
+    prediction_stock(graph, data_loader)
 
 
 if __name__ == '__main__':
