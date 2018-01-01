96381fa34282d399b06afbe42c864f7a0380e41b
LikeJust <diddydwns135@gmail.com>
2018-01-01 18:11:15 +0900

    test fn + test list read

--- stage4/stock_reg.py
+++ stage4/stock_reg.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 import os
 
 flag = tf.app.flags
-flag.DEFINE_integer('iterations', 2000, 'iterations')
+flag.DEFINE_integer('iterations', 1000, 'iterations')
 flag.DEFINE_integer('seq_length', 7, 'seq_length')
 flag.DEFINE_string('csv_path', 'csv_data', 'csv data path')
+flag.DEFINE_string('model_name', 'rnn', 'rnn model name')
 FLAGS = flag.FLAGS
+
+f = open('submission.txt', 'w', encoding='utf-8', newline='\n')
 
 
 def visual_graph(test_y, test_predict):
@@ -23,30 +26,54 @@ def train_fn(sess, graph, loader):
     sess.run(tf.global_variables_initializer())
     train_x, train_y = loader.get_train()
 
-    for i in range(FLAGS.iterations):
+    for i in range(1, FLAGS.iterations + 1):
         _, step_loss = graph.train(train_x, train_y)
-        print("[step: {}] loss: {}".format(i, step_loss))
+
+        if i % 50 == 0:
+            print("[step: {}] loss: {}".format(i, step_loss))
 
 
-def prediction_stock(graph, loader):
+def test_fn(graph, loader):
     test_x, test_y = loader.get_test()
     test_prediction = graph.test(test_x)
     visual_graph(test_y, test_prediction)
+
+
+def prediction_stock(graph, loader):
+    # test_fn(graph, loader)
 
     last_data = loader.get_last()
     last_prediction = graph.test(last_data)
     prediction, direction = loader.last_data_process(last_prediction)
 
-    print(prediction, direction)
+    return prediction, direction
+
+
+def write_file(value, direction):
+    f.write('{} {}\n'.format(direction, value))
+
+
+def read_test_list():
+    lines = open('data/grading.input.txt', 'r', encoding='utf-8', newline='')
+    test_names = list(map(lambda x: x[:-1], lines))
+    lines.close()
+
+    return test_names
 
 
 def main(_):
     sess = tf.Session()
-    graph = RnnModel(sess, 'rnn')
-    data_loader = DataLoader('csv_data/gears.csv', FLAGS)
+    graph = RnnModel(sess, FLAGS.model_name)
+    files = read_test_list()
 
-    train_fn(sess, graph, data_loader)
-    prediction_stock(graph, data_loader)
+    for file in files:
+        print(file)
+        data_loader = DataLoader(os.path.join(FLAGS.csv_path, file) + '.csv', FLAGS)
+        train_fn(sess, graph, data_loader)
+        v, d = prediction_stock(graph, data_loader)
+        write_file(v, d)
+
+    f.close()
 
 
 if __name__ == '__main__':
